7e8e16653a72c5678e4e61788956881c10b24bbd
phpontes <ppontes95@hotmail.com>
2025-08-28 13:33:36 -0300

    using the alternative dependency injection syntax

--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,16 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
+import { TasksService } from '../tasks.service';
 
 @Component({
-    selector: 'app-tasks-list',
-    templateUrl: './tasks-list.component.html',
-    styleUrl: './tasks-list.component.css',
-    imports: [TaskItemComponent]
+  selector: 'app-tasks-list',
+  templateUrl: './tasks-list.component.html',
+  styleUrl: './tasks-list.component.css',
+  imports: [TaskItemComponent],
 })
 export class TasksListComponent {
+  private tasksService = inject(TasksService);
   selectedFilter = signal<string>('all');
-  tasks = [];
+  tasks = this.tasksService.allTasks;
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
